795ec133ec5f2e3515228ace548abcff776a0c9d
Aayush Bhurtel <aayushbhurtel@gmail.com>
2022-03-27 11:42:56 -0500

    deleted records

--- tbldefs/Login.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [Login] (
-  [LoginID] AUTOINCREMENT CONSTRAINT [PrimaryKey] PRIMARY KEY UNIQUE NOT NULL,
-  [Username] VARCHAR (255),
-  [Password] VARCHAR (255),
-  [PlanLevel] LONG 
-)
