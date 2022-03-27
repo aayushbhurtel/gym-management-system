bdf7837cd9a686d17409ea2a52639eb6d9d265e7
Aayush Bhurtel <aayushbhurtel@gmail.com>
2022-03-27 11:43:52 -0500

    created new folder with access file

---
+++ Access/tbldefs/Member.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [Member] (
+  [ID] AUTOINCREMENT CONSTRAINT [PrimaryKey] PRIMARY KEY UNIQUE NOT NULL,
+  [MemberFirstName] VARCHAR (255),
+  [MemberMiddleName] VARCHAR (255),
+  [MemberLastName] VARCHAR (255),
+  [MemberEmail] LONGTEXT ,
+  [MemberPhone] COUNTER ,
+  [MemberGender] VARCHAR (255),
+  [MemberDOB] DATETIME ,
+  [MemberStreet] VARCHAR (255),
+  [MemberCity] VARCHAR (255),
+  [MemberState] VARCHAR (2),
+  [MemberZip] VARCHAR (5)
+)
